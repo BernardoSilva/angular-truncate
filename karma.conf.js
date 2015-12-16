9b6bafa34d2dc475be28c9f1d98a6482bf8b3afb
Oleksandr Savchenko <traveltino@gmail.com>
2015-12-16 12:56:55 +0200

    add editorconfig

--- karma.conf.js
+++ karma.conf.js
@@ -5,112 +5,110 @@
 
 var configuration = {
     // other things
- 
-    
 
 
-	// base path, that will be used to resolve files and exclude
-	// basePath: './..',
+    // base path, that will be used to resolve files and exclude
+    // basePath: './..',
 
-	// frameworks to use
-	frameworks: ['jasmine'],
+    // frameworks to use
+    frameworks: ['jasmine'],
 
-	// list of files / patterns to load in the browser
-	files: [
-	   'test/lib/angular/angular.min.js',
-	   'test/lib/angular/angular-mocks.js',
-	  'src/*.js',
-	  'test/*.js'
-	],
+    // list of files / patterns to load in the browser
+    files: [
+        'test/lib/angular/angular.min.js',
+        'test/lib/angular/angular-mocks.js',
+        'src/*.js',
+        'test/*.js'
+    ],
 
-	// list of files to exclude
-	exclude: [],
+    // list of files to exclude
+    exclude: [],
 
-	preprocessors: {
-      'src/*.js': ['coverage']
+    preprocessors: {
+        'src/*.js': ['coverage']
     },
 
-	// use dots reporter, as travis terminal does not support escaping sequences
-	// possible values: 'dots', 'progress', 'junit'
-	// CLI --reporters progress
-	// 'coverage',
-	reporters: ['progress', 'coverage', 'dots'],
+    // use dots reporter, as travis terminal does not support escaping sequences
+    // possible values: 'dots', 'progress', 'junit'
+    // CLI --reporters progress
+    // 'coverage',
+    reporters: ['progress', 'coverage', 'dots'],
 
-	coverageReporter: {
-      // specify a common output directory
-      dir: 'build/reports/coverage',
-      reporters: [
-        // reporters not supporting the `file` property
-        { type: 'html', subdir: 'report-html' },
-        { type: 'lcov', subdir: 'report-lcov' },
-        // reporters supporting the `file` property, use `subdir` to directly
-        // output them in the `dir` directory
-        { type: 'cobertura', subdir: '.', file: 'cobertura.txt' },
-        { type: 'lcovonly', subdir: '.', file: 'report-lcovonly.txt' },
-        { type: 'teamcity', subdir: '.', file: 'teamcity.txt' },
-        { type: 'text', subdir: '.', file: 'text.txt' },
-        { type: 'text-summary', subdir: '.', file: 'text-summary.txt' },
-      ]
+    coverageReporter: {
+        // specify a common output directory
+        dir: 'build/reports/coverage',
+        reporters: [
+            // reporters not supporting the `file` property
+            {type: 'html', subdir: 'report-html'},
+            {type: 'lcov', subdir: 'report-lcov'},
+            // reporters supporting the `file` property, use `subdir` to directly
+            // output them in the `dir` directory
+            {type: 'cobertura', subdir: '.', file: 'cobertura.txt'},
+            {type: 'lcovonly', subdir: '.', file: 'report-lcovonly.txt'},
+            {type: 'teamcity', subdir: '.', file: 'teamcity.txt'},
+            {type: 'text', subdir: '.', file: 'text.txt'},
+            {type: 'text-summary', subdir: '.', file: 'text-summary.txt'}
+        ]
     },
 
-	// web server port
-	// CLI --port 9876
-	port: 9876,
+    // web server port
+    // CLI --port 9876
+    port: 9876,
 
-	// cli runner port
-	// CLI --runner-port 9100
-	runnerPort: 9100,
+    // cli runner port
+    // CLI --runner-port 9100
+    runnerPort: 9100,
 
-	// enable / disable colors in the output (reporters and logs)
-	// CLI --colors --no-colors
-	colors: true,
+    // enable / disable colors in the output (reporters and logs)
+    // CLI --colors --no-colors
+    colors: true,
 
-	// level of logging
-	// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-	// CLI --log-level debug
-	//logLevel: LOG_INFO,
+    // level of logging
+    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+    // CLI --log-level debug
+    //logLevel: LOG_INFO,
 
-	// enable / disable watching file and executing tests whenever any file changes
-	// CLI --auto-watch --no-auto-watch
-	autoWatch: true,
+    // enable / disable watching file and executing tests whenever any file changes
+    // CLI --auto-watch --no-auto-watch
+    autoWatch: true,
 
-	// Start these browsers, currently available:
-	// - Chrome
-	// - ChromeCanary
-	// - Firefox
-	// - Opera
-	// - Safari (only Mac)
-	// - PhantomJS
-	// - IE (only Windows)
-	// CLI --browsers Chrome,Firefox,Safari
-	browsers: ['Chrome'],
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera
+    // - Safari (only Mac)
+    // - PhantomJS
+    // - IE (only Windows)
+    // CLI --browsers Chrome,Firefox,Safari
+    browsers: ['Chrome'],
 
-	// If browser does not capture in given timeout [ms], kill it
-	// CLI --capture-timeout 5000
-	captureTimeout: 5000,
+    // If browser does not capture in given timeout [ms], kill it
+    // CLI --capture-timeout 5000
+    captureTimeout: 5000,
 
-	// Auto run tests on start (when browsers are captured) and exit
-	// CLI --single-run --no-single-run
-	singleRun: false,
+    // Auto run tests on start (when browsers are captured) and exit
+    // CLI --single-run --no-single-run
+    singleRun: false,
 
-	// report which specs are slower than 500ms
-	// CLI --report-slower-than 500
-	reportSlowerThan: 500,
+    // report which specs are slower than 500ms
+    // CLI --report-slower-than 500
+    reportSlowerThan: 500,
 
-	customLaunchers: {
+    customLaunchers: {
         ChromeNoSandbox: {
-        	base: 'Chrome',
-        	flags: ['--no-sandbox'] 
-    	}
-    },
+            base: 'Chrome',
+            flags: ['--no-sandbox']
+        }
+    }
 
 };
- 
+
 if (process.env.TRAVIS) {
     configuration.browsers = ['ChromeNoSandbox'];
 }
- 
+
 module.exports = function (config) {
 
-config.set(configuration);
+    config.set(configuration);
 };
